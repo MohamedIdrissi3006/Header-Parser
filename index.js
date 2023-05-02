ba41478b8915116d063f1f4df101b170bcd0079b
Replit user <>
2023-05-02 02:37:04 +0000

    inital commit

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+const ip = req.socket.remoteAddress;
+  // console.log(JSON.stringify(req.headers));
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ipaddress : ip, language: language, software: software});
 });
 
 // listen for requests :)
